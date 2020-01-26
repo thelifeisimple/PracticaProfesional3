f4c9b9a8af9bce22f23ca4efd159b5536bcf7b7e
thelifeisimple <anamariazuluagavasquez@gmail.com>
2020-01-26 09:47:59 -0300

    Actualizacion Html pluss app.js

--- static/js/app.js
+++ static/js/app.js
@@ -1,42 +1,42 @@
-$(function() {
-    $('input[name="date-ingreso"]').daterangepicker({
-        singleDatePicker: true,
-        showDropdowns: true
-    });
-    $('input[name="date-egreso"]').daterangepicker({
-        singleDatePicker: true,
-        showDropdowns: true
-    });
-    $('input[name="date-baja"]').daterangepicker({
-        singleDatePicker: true,
-        showDropdowns: true
-    });
-     
+$(function(){
 
-    $("#situacion").click(function(){
-	    if($("#situacion").val() == "Suspención de Tareas"){
-	    	console.log("dentro de suspencion de tareas");
-	    	$('input[name="date-desde"]').attr("readonly", false);
-	    	$('input[name="date-hasta"]').attr("readonly", false);
-
-	    	$('.desde').show(); 
-	      	$('.hasta').show();
-
-	    	$('input[name="date-desde"]').daterangepicker({
-	        	singleDatePicker: true,
-	        	showDropdowns: true
-	    	});
-	      	$('input[name="date-hasta"]').daterangepicker({
-	        	singleDatePicker: true,
-	        	showDropdowns: true
-	    	});
-	    }else{
-	    	console.log("dentro de otra situacion");
-	    	$('input[name="date-desde"]').attr("readonly", true);
-	    	$('input[name="date-hasta"]').attr("readonly", true);
-
-	    	$('.desde').hide(); 
-	      	$('.hasta').hide();
-	    }
+	$('input[name="date-ingreso"]').daterangepicker({
+		singleDataPicker: true,
+		showDropdowns:true
 	});
+	$('input[name="date-egreso"]').daterangepicker({
+		singleDataPicker: true,
+		showDropdowns: true
+	});
+	$('input[name="date-baja"]').daterangepicker({
+		singleDataPicker: true,
+		showDropdowns:true
+	});
+
+	$("#situacion").click(function(){
+		if($("#situacion").val() == "Suspención de Tareas"){
+			console.log("dentro de suspencion de tareas");
+			$('input[name="date-desde"]').attr("readonly",false);
+			$('input[name="date-hasta"]').attr("readonly", false);
+
+			$('.desde').show();
+			$('.hasta').show();
+
+			$('input[name="date-desde"]').daterangepicker({
+				singleDataPicker: true,
+				showDropdowns: true
+			});
+			$('input[name="date-hasta"]').daterangepicker({
+				singleDataPicker: true,
+				showDropdowns: true
+			});
+		}else{
+			console.log("dentro de otra situacion");
+			$('input[name="date-desde"]').attr("readonly", true);
+			$('input[name="date-hasta"]').attr("readonly", true);
+
+			$('.desde').hide();
+			$('.hasta').hide();
+		}
+	});	
 });
